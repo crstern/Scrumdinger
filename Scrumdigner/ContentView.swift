4e1b5cf177fe660f6ab037566d4355957c9a154f
Cristian Stern <stern.cristi@gmail.com>
2023-10-18 13:53:13 +0300

    Initial Commit

---
+++ Scrumdigner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrumdigner
+//
+//  Created by Cristian Stern on 18.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrumdigner/ScrumdignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdignerApp.swift
+//  Scrumdigner
+//
+//  Created by Cristian Stern on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
